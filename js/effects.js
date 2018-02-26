c35e95d7a5095be58fb1ea1c7dd6e6c73a74f452
Barbara Stadnik <barbara1stadnik@gmail.com>
2018-02-26 12:39:06 +0100

    change styling in 'curiosities', add animation

--- js/effects.js
+++ js/effects.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-	$('.panel').on('click', function() {
-		$('.panel-heading').removeClass('panel_selected');
-		$(this).find('.panel-heading').toggleClass('panel_selected');
+	$('.panel-heading').on('click', function() {
+		$('.panel-heading').not(this).removeClass('panel_expanded');
+		$('.panel-body').removeClass('animate_in');
+		$(this).toggleClass('panel_expanded');
+		$(this).next().find('.panel-body').toggleClass('animate_in');
 	});
 });
