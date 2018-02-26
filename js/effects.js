9b045e9048f437c80bd61bebd5d510f12c51aa43
Barbara Stadnik <barbara1stadnik@gmail.com>
2018-02-26 14:45:27 +0100

    switch rwd of article links from css to jQuery

--- js/effects.js
+++ js/effects.js
@@ -1,8 +1,25 @@
-$(document).ready(function() {
-	$('.panel-heading').on('click', function() {
-		$('.panel-heading').not(this).removeClass('panel_expanded');
-		$('.panel-body').removeClass('animate_in');
-		$(this).toggleClass('panel_expanded');
-		$(this).next().find('.panel-body').toggleClass('animate_in');
-	});
+$(window).resize(function () {
+    adjustHeight('.articles__item');
 });
+
+$(document).ready(function(e) {
+    adjustHeight('.articles__item');
+
+    $('.panel-heading').on('click', accordionEffects);
+});
+
+function adjustHeight(elem){
+    var maxHeight = 0;
+    $(elem).css('height','auto');
+    $(elem).each(function(){
+       if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+    });
+    $(elem).height(maxHeight);
+}
+
+function accordionEffects() {
+	$('.panel-heading').not(this).removeClass('panel_expanded');
+	$('.panel-body').removeClass('animate_in');
+	$(this).toggleClass('panel_expanded');
+	$(this).next().find('.panel-body').toggleClass('animate_in');
+}
