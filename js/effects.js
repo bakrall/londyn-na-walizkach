4395255e1d6f63cf7e6c1a344b471ad8d70cd5e0
Barbara Stadnik <barbara1stadnik@gmail.com>
2018-02-26 21:40:01 +0100

    make sticky footer
    1. make sticky footer in case window screen is longer than the page
    2. change solution for removing left and right padding on
    .container-fluid (overflow: hidden spoiled the effect of sticky footer)
    3. correct look of buttons on viewport over 351px and below 480px

--- js/effects.js
+++ js/effects.js
@@ -1,9 +1,12 @@
 $(window).resize(function () {
     adjustHeight('.articles__item');
+    makeStickyFooter();
 });
 
 $(document).ready(function(e) {
     adjustHeight('.articles__item');
+    makeStickyFooter();
+
 
     $('.panel-heading').on('click', accordionEffects);
     $('.navbar a').on('click', scrollToAnchor);
@@ -39,4 +42,15 @@ function accordionEffects() {
 	$('.panel-body').removeClass('animate_in');
 	$(this).toggleClass('panel_expanded');
 	$(this).next().find('.panel-body').toggleClass('animate_in');
+}
+
+function makeStickyFooter() {
+    var footerPosition = $('footer').offset().top,
+        windowHeight = $(window).height(),
+        footerHeight = $('footer').height();
+
+    if (footerPosition < windowHeight) {
+        $('footer').offset({top: windowHeight - footerHeight});
+        $('.container-fluid').height(windowHeight);
+    }
 }
